1d74e8bc5c5e648d5aa1b4e846044ea35b67bdf6
LilaPotter <Karla.anchondo@gmail.com>
2022-05-29 22:10:53 -0600

    soft scroll

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){/*cambiar una clase cuando se produzca el scroll vertical*/
     $(window).on("scroll",function(){
+        //Cambio de header
         let posicion =$("#pr").offset(); /*esto me va a dar el valor de la posicion del scroll*/
         if($(window).scrollTop()>=posicion.top){
             $("header").removeClass("transparente");
@@ -7,8 +8,18 @@ $(document).ready(function(){/*cambiar una clase cuando se produzca el scroll ve
             $("header").addClass("transparente");
         }
     })
+    //Accionamiento del menú lateral
     $("#burger").click(function(){
         $("#burger").toggleClass("abierto");
         $(".menu").toggleClass("abierto");
+    })
+    // smooth scroll
+    $("a").on("click", function(){
+        if(this.hash !==""){
+            let hash = this.hash;
+            $("html,body").animate({
+                scrollTop: $(hash).offset().top
+            }, 800) //milisegundos
+        }
     })
 })
